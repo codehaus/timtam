d9902b059d0d453061f1a5ea3d39c7b9918d79c9
zohar <zohar@92f5660f-b372-0410-b680-f6ab4520eea6>
2004-10-23 17:00:48 +0000

    test plugin containing all unit tests for timtam - created

---
+++ TimTamTests/src/org/codehaus/timtam/TimTamAbstractTestCase.java
@@ -0,0 +1,32 @@
+/*
+ * Created on Oct 23, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.codehaus.timtam;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Sarah
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class TimTamAbstractTestCase extends TestCase{
+	
+	/* open the timtam perspective, 
+	 * and hook up a mook confluence server 
+	 * 
+	 */
+	protected void setUp() throws Exception {
+		
+	}
+
+	/* Disconect from the mock timtam server and
+	 * close the timtam perspective
+	 */
+	protected void tearDown() throws Exception {
+	}
+}
